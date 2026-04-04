6b5bc1a56270d1125859a81dbb502d32d0aebaa1
SukeSuke <79008036+sukesuke-code@users.noreply.github.com>
2026-04-05 06:14:59 +0900

    configure Vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://microwave-show-backend.lolololololol.workers.dev',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   optimizeDeps: {
     include: ['react', 'react-dom', 'lucide-react'],
   },
@@ -20,3 +29,4 @@ export default defineConfig({
     },
   },
 });
+
